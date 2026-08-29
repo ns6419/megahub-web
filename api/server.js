0383ddb9d9268d4d23b939afd27fb76748be440e
HADI <ns6419740@gmail.com>
2026-08-30 03:09:39 +0500

    Update server.js

--- api/server.js
+++ api/server.js
@@ -1,10 +1,6 @@
 const express = require('express');
-const path = require('path');
 const app = express();
 
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../public/index.html'));
-});
+app.use(express.static('public'));
 
 module.exports = app;
-        
